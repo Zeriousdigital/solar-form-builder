2ebfe57ec626848bc47910b3739fc2061a139a4a
Zeriousdigital <zeriousdigital@gmail.com>
2026-06-23 18:43:14 +0100

    setup Vercel both services

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    outDir: '../backend/public',
-    emptyOutDir: true
-  },
   server: {
     port: 3000,
     proxy: {
